dda60012629a16caa1b6cf477878ff3455e077b2
aboubamba <aboubacrbamba@gmail.com>
2022-03-21 20:29:38 +0000

    jwt-Aut initialise

--- src/app/auth/containers/login/login.component.ts
+++ src/app/auth/containers/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 
@@ -16,8 +16,8 @@ export class LoginComponent implements OnInit {
 
   ngOnInit() {
     this.loginForm = this.formBuilder.group({
-      username: [''],
-      password: ['']
+      username: ['', Validators.required],
+      password: ['', Validators.required]
     });
   }
 
